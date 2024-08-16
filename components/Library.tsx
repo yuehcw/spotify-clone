ad9e3d030326dd83d88e69ff1aeafe0124e0e328
Yueh Chun Wu <yuehcw@uci.edu>
2024-08-16 13:58:33 -0700

    Finish Project

---
+++ components/Library.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types";
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { TbPlaylist } from "react-icons/tb";
+import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
+
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+
+  const onPlay = useOnPlay(songs);
+
+  const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    // TODO: Check for subscription
+
+    return uploadModal.onOpen();
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex item-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist className="text-neutral-400" size={26} />
+          <p className="text-neutral-400 font-medium text-md">Your Library</p>
+        </div>
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+      </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((item) => (
+          <MediaItem
+            onClick={(id: string) => onPlay(id)}
+            key={item.id}
+            data={item}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Library;
